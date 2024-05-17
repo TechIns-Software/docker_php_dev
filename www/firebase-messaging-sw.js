b657d20955f7c3a09e041fcdf2d01dd575b5e076
pcmagas <pcmagas@disroot.org>
2024-05-17 10:27:53 +0300

    Initialize service worker.

--- www/firebase-messaging-sw.js
+++ www/firebase-messaging-sw.js
@@ -1,1 +1,13 @@
+importScripts("https://172.161.10.2/dist/firebase-messaging-sw.js");
 
+const firebaseConfig = {
+    apiKey: "AIzaSyAX__6udYHxrNuqFZfES_ZszobrF3PVUZY",
+    authDomain: "testproject-f9304.firebaseapp.com",
+    projectId: "testproject-f9304",
+    storageBucket: "testproject-f9304.appspot.com",
+    messagingSenderId: "1032769586386",
+    appId: "1:1032769586386:web:ee4867982d4adac4d6990b",
+    measurementId: "G-QL0YWYPC3J"
+};
+
+self.initServiceWorker(firebaseConfig);
--- www/index.js
+++ www/index.js
@@ -4,14 +4,14 @@ const firebaseConfig = {
     projectId: "testproject-f9304",
     storageBucket: "testproject-f9304.appspot.com",
     messagingSenderId: "1032769586386",
-    appId: "1:1032769586386:web:8ec9f6b7515cedd4d6990b",
-    measurementId: "G-HPQ1XEEV04"
+    appId: "1:1032769586386:web:ee4867982d4adac4d6990b",
+    measurementId: "G-QL0YWYPC3J"
 };
+  
 
 const vapidKey = "BABCEIwVE0n40VSzoLQ8dpudZig_NBM6C87v1dkJP-qLpTcZJkICLNQLGGTJ2FLJHMgJIEPqQzqXnd8cYZY0b5o";
 
 // Initialize Firebase  
 const wokerUrl = './firebase-messaging-sw.js';
 
-//        firebaseLib.pushNotificationInit(config,vapidKey,"",workerUrl)
 firebaseLib.pushNotificationInit(firebaseConfig,vapidKey,"",wokerUrl,true)
