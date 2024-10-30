69083d92f7909d79e4aac3fcea425404bbdd5385
pcmagas <pcmagas@disroot.org>
2024-10-30 15:28:55 +0200

    Entrypoint for elasticsearch.

---
+++ utils/entrypoint_elastic.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ls -l /usr/share/elasticsearch
+mkdir -p /usr/share/elasticsearch/data
+chown -R 1000:1000 /usr/share/elasticsearch/data
+
+su - elasticsearch /usr/local/bin/docker-entrypoint.sh "$@"
