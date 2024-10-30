cc235c46b04d1878a91c148fe6e2d9d3622daba6
pcmagas <pcmagas@disroot.org>
2024-10-30 16:08:36 +0200

    Preserve Environment

--- utils/entrypoint_elastic.sh
+++ utils/entrypoint_elastic.sh
@@ -4,4 +4,4 @@ ls -l /usr/share/elasticsearch
 mkdir -p /usr/share/elasticsearch/data
 chown -R 1000:1000 /usr/share/elasticsearch/data
 
-su - elasticsearch /usr/local/bin/docker-entrypoint.sh "$@"
+su -m elasticsearch /usr/local/bin/docker-entrypoint.sh "$@"
